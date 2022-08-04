16eb24de80fcb67cd35eed4989915e78962719db
neubasi <neubauersim@gmail.com>
2022-08-04 19:39:20 +0200

    update

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
 from datetime import datetime
-from flask_talisman import Talisman
 
 app = Flask(__name__)
 CORS(app)
-Talisman(app)
 
 
 @app.route('/getData')
